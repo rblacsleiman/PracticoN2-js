36485fc8c7cb3150acd18567b1a50695192ceab7
Rosario Blac Sleiman <rblacsleiman@gmail.com>
2023-03-11 20:38:37 -0300

    Se realiza ejercicio doce

---
+++ ejercicio12.js
@@ -0,0 +1,9 @@
+
+//Realiza un script que genere un número aleatorio entre 1 y 99
+
+
+let num = Math.floor(Math.random() * 99) + 1;
+
+document.write("El número aleatorio generado es: " + num);
+
+// cuando se presiona enter genera otro numero aleatoriamente entre el rango indicado
